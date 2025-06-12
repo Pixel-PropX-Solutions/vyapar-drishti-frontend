b60a67058de8ca97fbec4d25459c943d0c5d0aec
TOHID KHAN <tohidkhan1193407@gmail.com>
2025-06-12 21:10:52 +0530

    Refactor: Remove stockist reducer and related imports from the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,18 +6,9 @@ import productReducer from "./reducers/productReducer";
 import categoryReducer from "./reducers/categoryReducer";
 import inventoryReducer from "./reducers/inventoryReducer";
 import invoiceReducer from "./reducers/invoiceReducer";
-import stockistReducer from "./reducers/stockistReducer";
-<<<<<<< HEAD
 import customerReducer from "./reducers/customersReducer";
 import accountingGroupReducer from "./reducers/accountingGroupReducer";
 import inventoryGroupReducer from "./reducers/inventoryGroupReducer";
-=======
-import creditorReducer from "./reducers/creditorsLedgerReducer";
-import groupReducer from "./reducers/groupReducer";
->>>>>>> 5c5a27c4386cee0a00ece93aada65cbc238ee637
-import billingReducer from "./reducers/billingReducer";
-import shippingReducer from "./reducers/shippingReducer";
-// import orderReducer from "./reducers/orderReducer";
 import analyticsReducer from "./reducers/analyticsReducer";
 
 const store = configureStore({
@@ -26,25 +17,14 @@ const store = configureStore({
     category: categoryReducer,
     company: companyReducer,
 
-<<<<<<< HEAD
     accountingGroup: accountingGroupReducer,
     inventoryGroup: inventoryGroupReducer,
-    billing: billingReducer,
-    shipping: shippingReducer,
     customersLedger: customerReducer,
-=======
-    group: groupReducer,
-    billing: billingReducer,
-    shipping: shippingReducer,
-    creditorsLedger: creditorReducer,
->>>>>>> 5c5a27c4386cee0a00ece93aada65cbc238ee637
 
     auth: authReducer,
     user: userReducer,
     inventory: inventoryReducer,
     invoice: invoiceReducer,
-    stockist: stockistReducer,
-    // order: orderReducer,
     analytics: analyticsReducer,
   },
 });
